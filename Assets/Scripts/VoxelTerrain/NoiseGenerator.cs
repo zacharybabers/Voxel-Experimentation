4684eab5014633dd360309d1d4a2d14ccbd99926
Zach Babers <zacharybabers@gmail.com>
2021-09-10 02:19:35 -0400

    Initial Commit.

---
+++ Assets/Scripts/VoxelTerrain/NoiseGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public int seedNumber;
+    public float noiseScale;
+    public int noiseOctave;
+    public float noisePersistence;
+    public float noiseLacunarity;
+    public Vector2 noiseOffset;
+
+    private float[,] myNoiseMap;
+
+   
+
+    private float[,] heightMap()
+    {
+        return Noise.GenerateNoiseMap(width, height, seedNumber, noiseScale, noiseOctave, noisePersistence,
+            noiseLacunarity, noiseOffset);
+    }
+
+    private void Start()
+    {
+        myNoiseMap = heightMap();
+        foreach (float number in myNoiseMap)
+        {
+            print(number);
+        }
+    }
+}
