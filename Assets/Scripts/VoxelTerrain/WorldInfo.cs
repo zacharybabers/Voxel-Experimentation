29028901e57f03eb5fa80454594bf90bb1630da6
Zach Babers <zacharybabers@gmail.com>
2021-09-25 23:33:01 -0400

    About to start writing rendering stuff

---
+++ Assets/Scripts/VoxelTerrain/WorldInfo.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInfo : MonoBehaviour
+{
+    public int worldHeight {get; private set; }
+    
+}
