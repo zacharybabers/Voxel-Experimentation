a7fa6ca76473c46c805e5e9c7280d14eed354ed0
Zachary Babers <zacharybabers@gmail.com>
2021-12-05 04:05:33 -0500

    Added Noise Layer of "inner blocks" (stone basically) and stone below top layer.

--- Assets/Scripts/VoxelTerrain/Biome.cs
+++ Assets/Scripts/VoxelTerrain/Biome.cs
@@ -6,14 +6,22 @@ using Random = System.Random;
 
 public class Biome : MonoBehaviour
 {
+    
+    [SerializeField] private string name = "biome";
+    
     public int topBlock;
     public int innerBlock;
     
     [SerializeField] private List<FastNoiseUnity> additiveFastNoises;
     [SerializeField] private List<float> weights;
     [SerializeField] private List<FastNoiseUnity> multiplicativeFastNoises;
-    [SerializeField] private int groundLevel = 0;
     [SerializeField] private int globalScale = 32;
+    [SerializeField] private int topLayerHeight = 5;
+    [SerializeField] private FastNoiseUnity innerFastNoise;
+    [SerializeField] private int innerBlockBaseLevel = 0;
+    [SerializeField] private int innerScale = 16;
+    [SerializeField] private bool innerLayerAdditive;
+    
 
     private void Awake()
     {
@@ -23,7 +31,7 @@ public class Biome : MonoBehaviour
         }
     }
 
-    private int[,] GenerateHeightMap(Vector3 chunkCoord)
+    private int[,] GenerateHeightMap(Vector3 chunkCoord) //generate 'top block'/surface heightmap
     {
         int[,] initMap = new int[WorldInfo.chunkSize, WorldInfo.chunkSize];
 
@@ -53,10 +61,29 @@ public class Biome : MonoBehaviour
 
         return initMap;
     }
+    
+    //private int GenerateInnerHeightMap    //generate 'inner block'/stone heightmap
+    private int[,] GenerateInnerHeightMap(Vector3 chunkCoord)
+    {
+        int[,] initMap = new int[WorldInfo.chunkSize, WorldInfo.chunkSize];
+        for (int blockX = 0; blockX < WorldInfo.chunkSize; blockX++)
+        {
+            for (int blockY = 0; blockY < WorldInfo.chunkSize; blockY++)
+            {
+                float result = innerFastNoise.fastNoise.GetNoise(chunkCoord.x * WorldInfo.chunkSize + blockX,chunkCoord.y * WorldInfo.chunkSize + blockY) * innerScale + innerBlockBaseLevel;
+                initMap[blockX, blockY] = (int) result;
+            }
+        }
+
+        return initMap;
+    }
+    
+    //private int[,,] Process3DNoise //go through 3d noise functions w given thresholds and turn blocks to air for caves, or do reverse for overhangs and things
 
     public int[,,] GenerateChunkAtlas(Vector3 chunkCoord)
     {
         var heightMap = GenerateHeightMap(chunkCoord);
+        var innerHeightMap = GenerateInnerHeightMap(chunkCoord);
 
         var chunkAtlas = new int[WorldInfo.chunkSize, WorldInfo.chunkSize, WorldInfo.chunkSize];
 
@@ -66,9 +93,27 @@ public class Biome : MonoBehaviour
             {
                 for (int k = 0; k < WorldInfo.chunkSize; k++)
                 {
-                    if (chunkCoord.z * WorldInfo.chunkSize + k <= heightMap[i, j])
+                    var thisGlobalHeight = (int) chunkCoord.z * WorldInfo.chunkSize + k;
+
+                    if (thisGlobalHeight <= heightMap[i, j] - topLayerHeight)
+                    {
+                        chunkAtlas[i, j, k] = innerBlock;
+                    }
+                    else if (thisGlobalHeight <= heightMap[i, j])
                     {
                         chunkAtlas[i, j, k] = topBlock;
+                    }
+
+                    if (thisGlobalHeight <= innerHeightMap[i, j])
+                    {
+                        if (innerLayerAdditive)
+                        {
+                            chunkAtlas[i, j, k] = innerBlock;
+                        }
+                        else if (chunkAtlas[i, j, k] == topBlock)
+                        {
+                            chunkAtlas[i, j, k] = innerBlock;
+                        }
                     }
                 }
             }
