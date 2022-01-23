b890791259d715ac216d62097cf81bfdaf46b9ab
Zachary Babers <zacharybabers@gmail.com>
2022-01-22 20:39:08 -0500

    Work on LOD
    Beginning work on Initial LOD implementation. Simple skip and expand method.

--- Assets/Scripts/VoxelTerrain/CulledMeshBuilder.cs
+++ Assets/Scripts/VoxelTerrain/CulledMeshBuilder.cs
@@ -40,7 +40,7 @@ public class CulledMeshBuilder : MonoBehaviour
     public Mesh Build(ChunkData chunkData, ref int[,,] topChunk, ref int[,,] bottomChunk, ref int[,,] leftChunk,
         ref int[,,] rightChunk, ref int[,,] forwardChunk, ref int[,,] backChunk)
     {
-        timer = Time.realtimeSinceStartup;
+        //timer = Time.realtimeSinceStartup;
         
         int[,,] chunkArray = chunkData.voxelAtlas;
 
@@ -61,7 +61,7 @@ public class CulledMeshBuilder : MonoBehaviour
                 {
                     if (chunkArray[i, j, k] != 0)
                     {
-                        CreateQuads(chunkArray, i, j, k, ref topChunk, ref bottomChunk, ref leftChunk, ref rightChunk, ref forwardChunk, ref backChunk);
+                        CreateQuads(0, chunkArray, i, j, k, ref topChunk, ref bottomChunk, ref leftChunk, ref rightChunk, ref forwardChunk, ref backChunk);
                     }
                     
                 }
@@ -87,54 +87,112 @@ public class CulledMeshBuilder : MonoBehaviour
 
         
 
-        //Debug.Log(Time.realtimeSinceStartup - timer);
+        //Debug.Log("time: " + (Time.realtimeSinceStartup - timer));
         
         return mesh;
     }
 
-    private void CreateQuads(int[,,] chunkArray, int i, int j, int k, ref int[,,] topChunk, ref int[,,] bottomChunk, ref int[,,] leftChunk,
+    private void CreateQuads(int lodLevel, int[,,] chunkArray, int i, int j, int k, ref int[,,] topChunk, ref int[,,] bottomChunk, ref int[,,] leftChunk,
         ref int[,,] rightChunk, ref int[,,] forwardChunk, ref int[,,] backChunk)
     {
         int value = chunkArray[i, j, k];
+        int scrollFactor = (int) Mathf.Pow(2, lodLevel);
         
-        
-        if((i == 0 && leftChunk[length - 1, j, k] == 0) || (i != 0 && chunkArray[i-1, j, k] == 0))     //check back
+        if((i == 0 && leftChunk[length - 1, j, k] == 0) || (i != 0 && chunkArray[i-scrollFactor, j, k] == 0))     //check back
         {
-            CreateBackQuad(i, j, k, ref uvLookup[value].sideUVs);
+            CreateBackQuad(lodLevel, i, j, k, ref uvLookup[value].sideUVs);
         }
         
-        if((i == length - 1 && rightChunk[0, j, k] == 0) || (i != length - 1 && chunkArray[i+1, j, k] == 0))     //check front
+        if((i == length - 1 && rightChunk[0, j, k] == 0) || (i != length - 1 && chunkArray[i+scrollFactor, j, k] == 0))     //check front
         {
-            CreateFrontQuad(i, j, k, ref uvLookup[value].sideUVs);
+            CreateFrontQuad(lodLevel, i, j, k, ref uvLookup[value].sideUVs);
         }       
         
-        if((j == 0 && backChunk[i, width - 1, k] == 0) || (j!= 0 && chunkArray[i, j-1, k] == 0))     //check left
+        if((j == 0 && backChunk[i, width - 1, k] == 0) || (j!= 0 && chunkArray[i, j-scrollFactor, k] == 0))     //check left
         {
-            CreateLeftQuad(i, j, k, ref uvLookup[value].sideUVs);
+            CreateLeftQuad(lodLevel, i, j, k, ref uvLookup[value].sideUVs);
         }       
        
-        if((j == width - 1 && forwardChunk[i, 0, k] == 0) || (j != width - 1 && chunkArray[i, j+1, k] == 0))     //check right
+        if((j == width - 1 && forwardChunk[i, 0, k] == 0) || (j != width - 1 && chunkArray[i, j+scrollFactor, k] == 0))     //check right
         {
-            CreateRightQuad(i, j, k, ref uvLookup[value].sideUVs);
+            CreateRightQuad(lodLevel, i, j, k, ref uvLookup[value].sideUVs);
         }       
         
-        if( (k == 0 && bottomChunk[i, j, height - 1] == 0) || (k != 0 && chunkArray[i, j, k-1] == 0))     //check bottom
+        if( (k == 0 && bottomChunk[i, j, height - 1] == 0) || (k != 0 && chunkArray[i, j, k-scrollFactor] == 0))     //check bottom
         {
-            CreateBottomQuad(i, j, k, ref uvLookup[value].bottomUVs);
+            CreateBottomQuad(lodLevel, i, j, k, ref uvLookup[value].bottomUVs);
         }       
         
-        if((k == height - 1 && topChunk[i,j, 0] == 0) || (k != height - 1 && chunkArray[i, j, k+1] == 0))     //check top
+        if((k == height - 1 && topChunk[i,j, 0] == 0) || (k != height - 1 && chunkArray[i, j, k+scrollFactor] == 0))     //check top
         {
-            CreateTopQuad(i, j, k, ref uvLookup[value].topUVs);
+            CreateTopQuad(lodLevel, i, j, k, ref uvLookup[value].topUVs);
         }       
     }
+
+    public Mesh BuildLOD(int lodLevel, ChunkData chunkData, ref int[,,] topChunk, ref int[,,] bottomChunk, ref int[,,] leftChunk,
+        ref int[,,] rightChunk, ref int[,,] forwardChunk, ref int[,,] backChunk)
+    {
+        //timer = Time.realtimeSinceStartup;
+        
+        int[,,] chunkArray = chunkData.voxelAtlas;
+
+        length = chunkArray.GetLength(0);
+        width = chunkArray.GetLength(1);
+        height = chunkArray.GetLength(2);
+        
+        vertexData.Clear();
+        triangleData.Clear();
+        uvData.Clear();
+        
+        
+        for (int i = 0; i < length; i+= (int) Mathf.Pow(2, lodLevel))
+        {
+            for (int j = 0; j < width; j+= (int) Mathf.Pow(2, lodLevel))
+            {
+                for (int k = 0; k < height; k+= (int) Mathf.Pow(2, lodLevel))
+                {
+                    if (chunkArray[i, j, k] != 0)
+                    {
+                        CreateQuads(lodLevel, chunkArray, i, j, k, ref topChunk, ref bottomChunk, ref leftChunk, ref rightChunk, ref forwardChunk, ref backChunk);
+                    }
+                    
+                }
+            }
+        }
+
+        Mesh mesh = new Mesh();
+
+        for (int i = 0; i < vertexData.Count; i++)
+        {
+            Vector3 temp = vertexData[i];
+            Vector3 reordered = new Vector3(temp.x, temp.z, temp.y);
+            vertexData[i] = reordered;
+        }
+        
+        mesh.vertices = vertexData.ToArray();
+
+        mesh.uv = uvData.ToArray();
+
+        mesh.triangles = triangleData.ToArray();
+        
+        mesh.RecalculateNormals();
+
+        
+
+        //Debug.Log("time: " + (Time.realtimeSinceStartup - timer));
+        
+        
+        return mesh;
+    }
    
-    private void CreateTopQuad(int i,int j,int k, ref QuadUVs topUVs)
+    private void CreateTopQuad(int lodLevel, int i,int j,int k, ref QuadUVs topUVs)
     {
         for (int x = vertexData.Count; x < vertexData.Count + 6; x++)
         {
             triangleData.Add(x);
         }
+
+        
         
         Vector3 createdVertex = new Vector3(i + 1,j,k + 1);     //top left
         Vector3 createdVertex2 = new Vector3(i,j + 1,k + 1);   //bottom right
@@ -160,7 +218,7 @@ public class CulledMeshBuilder : MonoBehaviour
         uvData.Add(topUVs.bottomLeft);
     }
     
-    private void CreateLeftQuad(int i,int j,int k, ref QuadUVs sideUVs)
+    private void CreateLeftQuad(int lodLevel, int i,int j,int k, ref QuadUVs sideUVs)
     {
         for (int x = vertexData.Count; x < vertexData.Count + 6; x++)
         {
@@ -190,7 +248,7 @@ public class CulledMeshBuilder : MonoBehaviour
         uvData.Add(sideUVs.bottomRight);
         uvData.Add(sideUVs.bottomLeft);
     }
-    private void CreateRightQuad(int i,int j,int k, ref QuadUVs sideUVs)
+    private void CreateRightQuad(int lodLevel, int i,int j,int k, ref QuadUVs sideUVs)
     {
         for (int x = vertexData.Count; x < vertexData.Count + 6; x++)
         {
@@ -220,7 +278,7 @@ public class CulledMeshBuilder : MonoBehaviour
         uvData.Add(sideUVs.bottomRight);
         uvData.Add(sideUVs.bottomLeft);
     }
-    private void CreateFrontQuad(int i,int j,int k, ref QuadUVs sideUVs)
+    private void CreateFrontQuad(int lodLevel, int i,int j,int k, ref QuadUVs sideUVs)
     {
        
         
@@ -252,7 +310,7 @@ public class CulledMeshBuilder : MonoBehaviour
         uvData.Add(sideUVs.bottomRight);
         uvData.Add(sideUVs.bottomLeft);
     }
-    private void CreateBackQuad(int i,int j,int k, ref QuadUVs sideUVs)
+    private void CreateBackQuad(int lodLevel, int i,int j,int k, ref QuadUVs sideUVs)
     {
         for (int x = vertexData.Count; x < vertexData.Count + 6; x++)
         {
@@ -282,7 +340,7 @@ public class CulledMeshBuilder : MonoBehaviour
         uvData.Add(sideUVs.bottomRight);
         uvData.Add(sideUVs.bottomLeft);
     }
-    private void CreateBottomQuad(int i,int j,int k, ref QuadUVs bottomUVs)
+    private void CreateBottomQuad(int lodLevel, int i,int j,int k, ref QuadUVs bottomUVs)
     {
         for (int x = vertexData.Count; x < vertexData.Count + 6; x++)
         {
